658a81ca73e96cdf737e918f971f0f87d93a4ada
zhenghanlee <hanhan_1999@hotmail.com>
2022-12-11 14:06:57 +0800

    Add userscript

---
+++ userscript.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Open Kattis Dark Mode
+// @version      0.1
+// @description  Dark mode for Open Kattis
+// @author       SolarRabbit
+// @match        https://open.kattis.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @resource     darktheme https://raw.githubusercontent.com/solarrabbit99/kattis-dark/master/darktheme.css
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  var style = GM_getResourceText("darktheme");
+
+  GM_addStyle(style);
+})();
